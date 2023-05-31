4e5ececd627ff2b98d1638560fca4a7a7610e005
AyimWilliam <odhisayim@gmail.com>
2023-05-31 06:51:57 +0300

    Completed assignment

--- lib/debug.py
+++ lib/debug.py
@@ -3,6 +3,15 @@
 from config import CONN, CURSOR
 from song import Song
 
+def reset_database():
+    Song.drop_table()
+    Song.create_table()
+    Song.create("Hello", "25")
+    Song.create("99 Problems", "The Black Album")
 
-if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+reset_database()
+
+import pytest; pytest.set_trace()
+
+# if __name__ == '__main__':
+#     import ipdb; ipdb.set_trace()
--- lib/song.py
+++ lib/song.py
@@ -1,15 +1,22 @@
-from config import CONN, CURSOR
 import sqlite3
 
 CONN = sqlite3.connect('music.db')
 CURSOR = CONN.cursor()
 
 class Song:
+    all = []
 
     def __init__(self, name, album):
         self.id = None
         self.name = name
         self.album = album
+
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS songs
+        """
+        CURSOR.execute(sql)
 
     @classmethod
     def create_table(cls):
@@ -35,6 +42,36 @@ class Song:
 
     @classmethod
     def create(cls, name, album):
-        song =Song(name,album)
+        song = Song(name, album)
         song.save()
         return song
+    
+    @classmethod
+    def new_from_db(cls, row):
+        song = cls(row[1], row[2])
+        song.id = row[0]
+        return song
+
+    @classmethod
+    def get_all(cls):
+        sql = """
+            SELECT *
+            FROM songs
+        """
+
+        all_rows = CURSOR.execute(sql).fetchall()
+
+        cls.all = [cls.new_from_db(row) for row in all_rows]
+        return cls.all
+    
+    @classmethod
+    def find_by_name(cls, name):
+        sql = """
+            SELECT *
+            FROM songs
+            WHERE name = ?
+            LIMIT 1
+        """
+        song = CURSOR.execute(sql, (name,)).fetchone()
+
+        return cls.new_from_db(song)
